5aa3e15e81eb3ccfe7e94a0c1b04e7bc496abbb2
Jackson Visser <jvisser@Jacksons-MacBook-Air.local>
2026-02-19 17:27:55 -0500

    Create js-fundies.js
    finish q1

---
+++ js-fundies.js
@@ -0,0 +1,55 @@
+//Module 3.1 
+
+//1. What are the results of these expressions
+
+("" + 1 + 0); // "10"
+
+""- 1 + 0 // -1
+
+true + false; // 1
+
+!true //false
+
+6 / "3" // 2
+
+"2" * "3" // 6 
+
+4 + 5 + "px" // "9px"
+
+
+"$" + 4 + 5 // "$45"
+
+
+"4" - 2 // 2
+
+
+"4px" - 2 // NaN
+
+
+" -9 " + 5 // " -9 5"
+
+
+" -9 " - 5  // -14
+
+
+null + 1 // 1
+
+
+undefined + 1 // NaN
+
+
+undefined == null // true
+
+undefined === null // false
+
+
+" \t \n" - 2 // -2 (on a new line)
+
+
+
+
+
+
+
+
+
