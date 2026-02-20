4fb98e8ff0b2bf602d3c5e636498c6c0c2e825c1
visserjackson-code <visserjackson@gmail.com>
2026-02-20 13:44:02 -0500

    set up intermediate ex 1 for next work sesh

---
+++ intermediate.js
@@ -0,0 +1,9 @@
+// Create a function that takes a string as a parameter and returns the string with the first
+// character of each word changed into a capital letter, as in the example below. Test it with
+// different strings.
+
+console.log(ucFirstLetters("los angeles") ) //Los Angeles
+
+const ucFirstLetters = (string) => {
+    //split into an array using space as delimiter
+}
