f425b635f5905637ab6cf89a01af729e7fcc300b
visserjackson-code <visserjackson@gmail.com>
2026-02-20 13:11:33 -0500

    done with 10

--- js-fundies.js
+++ js-fundies.js
@@ -248,12 +248,7 @@ cat2 = {...cat1, name: "Little", breed: "Large"};
 
 // 10. The following constructor function creates a new Person object with the given name and
 // age values.
-// a) Create a new person using the constructor function and store it in a variable
-// b) Create a second person using different name and age values and store it in a separate
-// variable
-// c) Print out the properties of each person object to the console
-// d) Rewrite the constructor function as a class called PersonClass and use it to create a
-// third person using different name and age values. Print it to the console as well.
+
 // e) Add a canDrive method to both the constructor function and the class that returns true
 // if the person is old enough to drive.
 
@@ -261,4 +256,32 @@ function Person(name, age) {
 this.name = name;
 this.age = age;
 this.human = true;
+this.canDrive = this.age >= 16;
 }
+
+// a) Create a new person using the constructor function and store it in a variable
+
+let KD = new Person("Kevin Durant", 37);
+
+// b) Create a second person using different name and age values and store it in a separate
+// variable
+let BG = new Person("Baby Gronk", 12);
+
+// c) Print out the properties of each person object to the console
+console.dir(KD);
+console.dir(BG);
+
+// d) Rewrite the constructor function as a class called PersonClass and use it to create a
+// third person using different name and age values. Print it to the console as well.
+class PersonClass {
+    
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.human = true;
+        this.canDrive = this.age >= 16 ;
+    }
+}
+
+let K9 = new PersonClass("Kenneth Walker III", 25);
+console.dir(K9);
