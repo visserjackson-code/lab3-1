c7f9d45cdeabc928253ecbe271d2c1a5ea0a2060
visserjackson-code <visserjackson@gmail.com>
2026-02-20 11:57:17 -0500

    finished question 9

--- js-fundies.js
+++ js-fundies.js
@@ -188,3 +188,40 @@ const printCity = (city) => {
 
 printCity(sydney);
 printCity(seattle);
+
+
+// 9. Use the following variables to understand how JavaScript stores objects by reference.
+
+// d) Print the original teamSports, dog1 and cat1 variables to the console. Have they
+// changed? Why?
+
+
+let teamSports = ['Hockey', 'Cricket', 'Volleyball'];
+let dog1 = 'Bingo';
+let cat1 = { name: 'Fluffy', breed: 'Siberian' };
+
+// a) Create a new moreSports variable equal to teamSports and add some new sport
+// values to it (using push and unshift)
+
+// let moreSports = teamSports;
+// moreSports.push("Basketball", "Gridiron Football", "Skiiing");
+// moreSports.unshift("Association Football");
+
+// b) Create a new dog2 variable equal to dog1 and give it a new value
+let dog2 = dog1;
+dog2 = 'Zucca';
+
+// c) Create a new cat2 variable equal to cat1 and change its name property
+
+// let cat2 = cat1;
+// cat2.name = "Little"
+
+// d) Print the original teamSports, dog1 and cat1 variables to the console. Have they
+// changed? Why? 
+console.log(teamSports, dog1, cat1); //they have all changed becuase this just copied the object reference
+
+// e) Change the way the moreSports and cat2 variables are created to ensure the
+// originals remain independent
+
+moreSports = ["Association Football", ...teamSports, "Basketball", "Gridiron Football", "Skiiing"];
+cat2 = {...cat1, name: "Little", breed: "Large"};
